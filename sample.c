856618563251d74414ae53648fdf600762956fe3
rohitumrao <rohitkumarumrao@gmail.com>
2013-09-01 09:13:32 +0600

    Create sample.c
    Enter the integer value according to its range in the given structure data type

---
+++ sample.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct dst
+  {
+      int a:1;
+      int b:2;
+      int c:3;
+      int d:4;
+      int e:5;
+      int f:6;
+      int g:7;
+      int h:8;
+      int i:9;
+      int j:10;
+      int k:11;
+      int ab:12;
+      int ac:13;
+      int ad:14;
+      int ae:15;
+      int af:16;
+      };
+      
+      typedef struct dst udt;
+      
+      
